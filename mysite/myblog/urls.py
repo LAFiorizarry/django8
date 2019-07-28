5b02c683aec1a5b1a17e5ade1395b897f6a489eb
lauraannf <lauraanng@gmail.com>
2019-07-28 17:41:43 -0400

    8-2

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import list_view, detail_view, add_model
+from myblog.feeds import LatestEntriesFeed
 
 urlpatterns = [
     path('', list_view, name="blog_index"),
     path('posts/<int:post_id>/', detail_view, name="blog_detail"),
     path('post/new/', add_model, name='post_new'),
+    path('post/feed/', LatestEntriesFeed())
 ]
